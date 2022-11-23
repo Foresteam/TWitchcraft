a7e624d468e31e954069f6c54f4655887d774669
Foresteam <heler1141@gmail.com>
2022-11-23 09:42:52 +0300

    Add all potions to table

--- Core/Liquids/ObsidianSkinPotion.cs
+++ Core/Liquids/ObsidianSkinPotion.cs
@@ -2,8 +2,8 @@ using Microsoft.Xna.Framework;
 
 // The "secret" potion. Should this be used for something else?
 namespace TWitchery.Liquids;
-class ObsidianSkin : Liquid {
+class ObsidianSkinPotion : Liquid {
 	public override Color Color => new Color(54, 44, 104);
 	public override Color? ColorSecondary => new Color(125, 110, 199);
-	public ObsidianSkin(float volume = 0) : base(volume) {}
+	public ObsidianSkinPotion(float volume = 0) : base(volume) {}
 }

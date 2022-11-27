61235b50e195e6616229b49f7abbc5dd9d7f0f4f
StarlightProg <942003@bk.ru>
2022-11-28 00:16:31 +0300

    EnergyDrainer Cauldron

--- Core/EnergyDrainer.cs
+++ Core/EnergyDrainer.cs
@@ -7,205 +7,184 @@ using System.Collections.Generic;
 using System.Linq;
 using System;
 
-namespace EnergyDrain
-{
-    public class EnergyDrainer
+namespace TWitchery;
+using Liquids;
+class EnergyDrainer<T> where T : ModTileEntity {
+	// private Tile _tile;
+	private float _yetToDrain;
+    private float _amountDrainFlora;
+	private int _x, _y;
+
+	public EnergyDrainer(float amountDrainFlora = 15) {		
+        _amountDrainFlora = amountDrainFlora;
+	}
+
+    public float DrainAltar(float amount,Player ply, int i, int j) {
+		
+		HelpMe.GetTileOrigin(ref i, ref j);
+		_x = i; _y = j;
+
+        _yetToDrain = amount;
+
+        foreach (var step in new Func<bool>[] { () => DrainPlayer(ply), DrainBiome, DrainFlora, DrainBlocks, DrainLivingForms }) ;
+
+        Main.NewText(_yetToDrain);
+		return _yetToDrain;
+    }
+
+    public float DrainCauldron(float amount, Player ply, LiquidInventory liquids) {
+		_yetToDrain = amount;
+
+		foreach (var step in new Func<bool>[] { () => DrainPlayer(ply), () => DrainLiquid(liquids) })
+			if (_yetToDrain > 0)
+				step();
+			else
+				break;
+		return _yetToDrain;
+	}
+    
+    private bool DrainPlayer(Player ply) {
+        var couldntTake = ply.GetModPlayer<TWitcheryPlayer>().TakeMana((int)_yetToDrain, useDeplition: false);
+        return couldntTake <= 0;
+    }
+
+    private bool DrainLiquid(LiquidInventory liquids) {
+        if (_yetToDrain == 0)
+            return true;
+        List<Liquid> remove = new();
+        foreach (Liquid liquid in liquids)
+            if (Tables.Common.energyLiquids.ContainsKey(liquid.GetType()))
+            {
+                var mpu = Tables.Common.energyLiquids[liquid.GetType()];
+                if (liquid.Volume < _yetToDrain / mpu)
+                {
+                    _yetToDrain -= liquid.Volume * mpu;
+                    remove.Add(liquid);
+                    continue;
+                }
+                liquid.Volume -= _yetToDrain / mpu;
+                return true;
+            }
+        foreach (var toRemove in remove)
+            liquids.Take(toRemove);
+        
+        return _yetToDrain > 0;
+    }
+
+	private bool DrainFlora() {
+		int radius = 10;
+		for (int t = _x - radius + 1; t < _x + radius; t++)
+			for (int o = _y - radius + 2; o < _y + radius; o++) {
+				//Main.NewText(Main.tile[t, o].TileType);
+				if (Main.tile[t, o].TileType == 2) {
+					//Main.NewText("Есть: "+ Main.tile[t, o].TileType);
+					_yetToDrain -= _amountDrainFlora;
+					WorldGen.ReplaceTile(t, o, TileID.Dirt, 0);
+				}
+				if (_yetToDrain < 0)
+					break;
+			}
+        return _yetToDrain > 0;
+    }
+
+	private bool DrainBlocks() {
+		//tile.HasTile();
+		int radius = 6;
+		for (int o = _y - radius + 2; o < _y + radius; o++)
+			for (int t = _x - radius + 1; t < _x + radius; t++) {
+				if (Main.tile[t, o].TileType == TileID.Dirt && Main.tile[t, o].HasTile) {
+					_yetToDrain -= 5;
+					WorldGen.ReplaceTile(t, o, TileID.Stone, 0);
+				}
+				if (_yetToDrain < 0)
+					break;
+			}
+        return _yetToDrain > 0;
+    }
+
+    private bool DrainBiome()
     {
-        private Tile tile;
-        private float amountLeft;
-        private int x;
-        private int y;
+        int radius = 10;
+        for (int t = _x - radius + 1; t < _x + radius; t++)
+        {
+            for (int o = _y - radius + 2; o < _y + radius; o++)
+            {
+                //Main.NewText(Main.tile[t, o].TileType);
+                if (Main.tile[t, o].TileType == 3)
+                {
+                    //Main.NewText("Есть: "+ Main.tile[t, o].TileType);
+                    _yetToDrain -= 25;
+                    WorldGen.KillTile(t, o);
+                }
+                if (_yetToDrain < 0)
+                {
+                    break;
+                }
+            }
+        }
+        return _yetToDrain > 0;
+    }
+
+    private bool DrainLivingForms()
+    {
         Vector2 pos;
+        pos = new Vector2(_x, _y);
 
-        public EnergyDrainer(int i, int j, float amount)
+
+        float maxDetectDistance = 400f;
+        NPC closestNPC = null;
+
+        // Using squared values in distance checks will let us skip square root calculations, drastically improving this method's speed.
+        float sqrMaxDetectDistance = maxDetectDistance * maxDetectDistance;
+
+        // Loop through all NPCs(max always 200)
+        for (int k = 0; k < Main.maxNPCs; k++)
         {
-
-            Player player = Main.LocalPlayer;
-            Tile tile = Main.tile[i, j];
-
-
-            x = i; y = j;
-
-            amountLeft = amount;
-
-            if (tile.TileFrameX < 16)
+            float maxDetectRadius = 1000f;
+            NPC target = Main.npc[k];
+            // Check if NPC able to be targeted. It means that NPC is
+            // 1. active (alive)
+            // 2. chaseable (e.g. not a cultist archer)
+            // 3. max life bigger than 5 (e.g. not a critter)
+            // 4. can take damage (e.g. moonlord core after all it's parts are downed)
+            // 5. hostile (!friendly)
+            // 6. not immortal (e.g. not a target dummy)
+            if (target.CountsAsACritter)
             {
-                x++;
-            }
+                float sqrDistanceToTarget = Vector2.DistanceSquared(
+                    new Vector2(target.Center.ToTileCoordinates().X, target.Center.ToTileCoordinates().Y),
+                    pos);
+                //Main.NewText("Центр мир: " + target.Center);
+                //Main.NewText("Центр экран?: " + target.Center.ToTileCoordinates());
 
-            if (tile.TileFrameY < 16)
-            {
-                y++;
-            }
-
-            if (tile.TileFrameX > 16)
-            {
-                x--;
-            }
-
-            if (tile.TileFrameY > 16)
-            {
-                y--;
-            }
-
-            pos = new Vector2(x, y);
-        }
-
-        public void Drain()
-        {
-
-            if (amountLeft > 0)
-            {
-                drainLivingForms();
-            }
-            if (amountLeft > 0)
-            {
-                drainBiome();
-            }
-            if (amountLeft > 0)
-            {
-                drainFlora();
-            }
-            if (amountLeft > 0)
-            {
-                drainBlocks();
-            }
-            Main.NewText(amountLeft);
-        }
-
-        private void drainFlora()
-        {
-            int radius = 10;
-            for (int t = x - radius + 1; t < x + radius; t++)
-            {
-                for (int o = y - radius + 2; o < y + radius; o++)
+                // Check if it is within the radius
+                if (sqrDistanceToTarget < sqrMaxDetectDistance)
                 {
-                    //Main.NewText(Main.tile[t, o].TileType);
-                    if (Main.tile[t, o].TileType == 2)
-                    {
-                        //Main.NewText("Есть: "+ Main.tile[t, o].TileType);
-                        amountLeft -= 15;
+                    Main.NewText("Есть монстр: ");
+                    sqrMaxDetectDistance = sqrDistanceToTarget;
+                    closestNPC = target;
+                    //target.life = 0;
+                    target.StrikeNPC(999, 0, 0);
+                    //target.CountsAsACritter
+                }
+            }
+            if (target.CanBeChasedBy())
+            {            
+                float sqrDistanceToTarget = Vector2.DistanceSquared(
+                    new Vector2(target.Center.ToTileCoordinates().X, target.Center.ToTileCoordinates().Y), pos);
 
-                        WorldGen.ReplaceTile(t, o, TileID.Dirt, 0);
-                    }
-                    if (amountLeft < 0)
-                    {
-                        break;
-                    }
+                // Check if it is within the radius
+                if (sqrDistanceToTarget < sqrMaxDetectDistance)
+                {
+                    sqrMaxDetectDistance = sqrDistanceToTarget;
+                    closestNPC = target;
+                    //target.life = 0;
+                    target.StrikeNPC(999, 0, 0);
+                    _yetToDrain -= 40;
+                    //target.CountsAsACritter
                 }
             }
         }
-
-        private void drainBlocks()
-        {
-            //tile.HasTile();
-            int radius = 6;
-            for (int o = y - radius + 2; o < y + radius; o++)
-            {
-                for (int t = x - radius + 1; t < x + radius; t++)
-                {
-                    if (Main.tile[t, o].TileType == TileID.Dirt && Main.tile[t, o].HasTile)
-                    {
-                        amountLeft -= 5;
-
-                        WorldGen.ReplaceTile(t, o, TileID.Stone, 0);
-                    }
-                    if (amountLeft < 0)
-                    {
-                        break;
-                    }
-                }
-            }
-        }
-
-        private void drainBiome()
-        {
-            int radius = 10;
-            for (int t = x - radius + 1; t < x + radius; t++)
-            {
-                for (int o = y - radius + 2; o < y + radius; o++)
-                {
-                    //Main.NewText(Main.tile[t, o].TileType);
-                    if (Main.tile[t, o].TileType == 3)
-                    {
-                        //Main.NewText("Есть: "+ Main.tile[t, o].TileType);
-                        amountLeft -= 25;
-                        WorldGen.KillTile(t, o);
-                    }
-                    if (amountLeft < 0)
-                    {
-                        break;
-                    }
-                }
-            }
-        }
-
-        private void drainLivingForms()
-        {
-            float maxDetectDistance = 400f;
-            NPC closestNPC = null;
-
-            // Using squared values in distance checks will let us skip square root calculations, drastically improving this method's speed.
-            float sqrMaxDetectDistance = maxDetectDistance * maxDetectDistance;
-
-            // Loop through all NPCs(max always 200)
-            for (int k = 0; k < Main.maxNPCs; k++)
-            {
-                float maxDetectRadius = 1000f;
-                NPC target = Main.npc[k];
-                // Check if NPC able to be targeted. It means that NPC is
-                // 1. active (alive)
-                // 2. chaseable (e.g. not a cultist archer)
-                // 3. max life bigger than 5 (e.g. not a critter)
-                // 4. can take damage (e.g. moonlord core after all it's parts are downed)
-                // 5. hostile (!friendly)
-                // 6. not immortal (e.g. not a target dummy)
-                if (target.CountsAsACritter)
-                {
-                    float sqrDistanceToTarget = Vector2.DistanceSquared(
-                        new Vector2(target.Center.ToTileCoordinates().X, target.Center.ToTileCoordinates().Y),
-                        pos);
-                    //Main.NewText("Центр мир: " + target.Center);
-                    //Main.NewText("Центр экран?: " + target.Center.ToTileCoordinates());
-
-                    // Check if it is within the radius
-                    if (sqrDistanceToTarget < sqrMaxDetectDistance)
-                    {
-                        Main.NewText("Есть монстр: ");
-                        sqrMaxDetectDistance = sqrDistanceToTarget;
-                        closestNPC = target;
-                        //target.life = 0;
-                        target.StrikeNPC(999, 0, 0);
-                        //target.CountsAsACritter
-                    }
-                }
-                if (target.CanBeChasedBy())
-                {
-
-                    // The DistanceSquared function returns a squared distance between 2 points, skipping relatively expensive square root calculations
-                    float sqrDistanceToTarget = Vector2.DistanceSquared(
-                        new Vector2(target.Center.ToTileCoordinates().X, target.Center.ToTileCoordinates().Y),
-                        pos);
-                    //Main.NewText("Центр мир: " + target.Center);
-                    //Main.NewText("Центр экран?: " + target.Center.ToTileCoordinates());
-
-                    // Check if it is within the radius
-                    if (sqrDistanceToTarget < sqrMaxDetectDistance)
-                    {
-                        sqrMaxDetectDistance = sqrDistanceToTarget;
-                        closestNPC = target;
-                        //target.life = 0;
-                        target.StrikeNPC(999, 0, 0);
-                        //target.CountsAsACritter
-                    }
-                }
-            }
-        }
-
-        //private void drainMonsters()
-        //{
-
-        //}
-
+        return _yetToDrain > 0;
     }
 }
